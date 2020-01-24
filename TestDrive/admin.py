ce5ff1f630325f7362928bc960b7e68aac7de064
gearbox <mtayrov@gmail.com>
2020-01-24 17:35:51 +0300

    Improve Models

--- TestDrive/admin.py
+++ TestDrive/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import SCPoint, Type, Message, Language
 # Register your models here.
+
+
+admin.site.register(SCPoint)
+admin.site.register(Type)
+admin.site.register(Message)
+admin.site.register(Language)
--- TestDrive/models.py
+++ TestDrive/models.py
@@ -3,8 +3,16 @@ from django.db import models
 # Create your models here.
 
 
-class Message(models.Model):
+class Language(models.Model):
     language = models.CharField(max_length=30, unique=True)
+    country = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.language
+
+
+class Message(models.Model):
+    language = models.ForeignKey(to=Language, on_delete=models.CASCADE)
     voice_message_text = models.CharField(max_length=200)
     audio_file_name = models.CharField(max_length=30)
     image_file_name = models.CharField(max_length=30)
@@ -14,28 +22,28 @@ class Message(models.Model):
 
 
 class Type(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    number = models.PositiveSmallIntegerField(unique=True)
-    description = models.TextField(max_length=500)
+    name = models.CharField(verbose_name='SpeedCam code name', max_length=100, unique=True)
+    number = models.PositiveSmallIntegerField(verbose_name='SpeedCam type number', unique=True)
+    description = models.TextField(verbose_name='SpeedCam description', max_length=200)
     message = models.ForeignKey(to=Message, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.description
+        return 'Type: ' + str(self.number) + '; ' + self.description
 
 
 class SCPoint(models.Model):
-    lon = models.DecimalField(max_digits=11, decimal_places=8)  # longitude X
-    lat = models.DecimalField(max_digits=11, decimal_places=8)  # latitude Y
-    type = models.ForeignKey(to=Type, on_delete=models.CASCADE)
-    speed = models.PositiveSmallIntegerField(default=0)
+    lon = models.DecimalField(verbose_name='Longitude (X)', max_digits=11, decimal_places=8, default=0)
+    lat = models.DecimalField(verbose_name='Latitude (Y)', max_digits=11, decimal_places=8, default=0)
+    type = models.ForeignKey(verbose_name='SpeedCam Type', to=Type, on_delete=models.CASCADE)
+    speed = models.PositiveSmallIntegerField(verbose_name='Speed limit', default=0)
     dirtype = models.PositiveSmallIntegerField(default=1)
-    direction = models.SmallIntegerField  # Should it be Only Positive (0<)?
-    add_date = models.DateTimeField('Date on which the SpeedCam was added')
-    priority = models.PositiveSmallIntegerField(default=0)
+    direction = models.SmallIntegerField(verbose_name='Direction from which SC catches', default=0)
     danger = models.BooleanField(default=False)
-    method = models.CharField(max_length=50)
+    priority = models.PositiveSmallIntegerField(default=0)
+    add_date = models.DateTimeField(verbose_name='Date on which the SpeedCam was added')
+    method = models.CharField(max_length=50, default='none')
     active = models.BooleanField(default=False)
     available = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.type
+        return 'ID: ' + str(self.id) + '; Type: ' + str(self.type.number) + '; ' + self.type.description
